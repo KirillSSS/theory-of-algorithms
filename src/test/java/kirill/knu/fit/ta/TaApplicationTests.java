a0e670d085af7759e2a530524b7743bf96de62d2
KirillSSS <savchuk.kirill.ua@gmail.com>
2021-04-09 02:25:59 +0300

    Lab2

--- src/test/java/kirill/knu/fit/ta/TaApplicationTests.java
+++ src/test/java/kirill/knu/fit/ta/TaApplicationTests.java
@@ -1,13 +1,26 @@
 package kirill.knu.fit.ta;
 
+import kirill.knu.fit.ta.lab2.EquationForLab2;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class TaApplicationTests {
 
-	@Test
+    @Autowired
+    EquationForLab2 equationForLab2;
+    
+        @Test
 	void contextLoads() {
 	}
+        
+        @Test
+        void testforlab2(){
+        assertEquals(Integer.MAX_VALUE,equationForLab2.equation(0,0,0,0));
+        assertEquals(2,equationForLab2.equation(0,4,0,2));
+        assertEquals(0.1,equationForLab2.equation(2,-3,1,3));
+        }
 
 }
